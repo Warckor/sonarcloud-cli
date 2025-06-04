70dd6560b9c50545055ae7f67787f6367052f01f
Miguel Gorostola <miguel.gorostola.servexternos@bancopichincha.es>
2025-06-04 23:53:45 +0200

    Refactor command structure: create new action files and remove obsolete ones

---
+++ cmd/actions/list.go
@@ -0,0 +1,18 @@
+package actions
+
+import (
+	"sonar-api/cmd/components"
+
+	"github.com/spf13/cobra"
+)
+
+func ListCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "list",
+		Short: "Recupera recursos desde SonarCloud",
+		Long:  "Este comando permite recuperar todos los recursos específicos desde SonarCloud, como proyectos, métricas, etc.",
+	}
+
+	cmd.AddCommand(components.ListProjectsCmd())
+	return cmd
+}
